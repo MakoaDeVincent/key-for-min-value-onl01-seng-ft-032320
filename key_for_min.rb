e15c37efd0e9af89b5de84904eee404e7e460e6f
Makoa DeVincent <makoadevincent@gmail.com>
2020-03-26 20:50:24 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,14 @@
 
 def key_for_min_value(name_hash)
 
+  low_item = nil
+  low_num = 1000
+
+  name_hash.each do |item, num|
+    if num < low_num
+      low_num = num
+      low_item = item
+    end
+  end
+  low_item
 end
